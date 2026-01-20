7c71e7925b54c7b4236f4b34cb8fb311e22e3102
marco-saretta-DTU <mcsr@dtu.dk>
2026-01-20 14:44:45 +0100

    Create testing branch

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+_TEST_ROOT = Path(__file__).parent
+_PROJECT_ROOT = _TEST_ROOT.parent
+_PATH_DATA = _PROJECT_ROOT / 'data'
--- tests/data/test_data.py
+++ tests/data/test_data.py
@@ -1,9 +1,9 @@
 from torch.utils.data import Dataset
-
 from mlops_loadconsumption.data import MyDataset
+from tests import _PATH_DATA
 
 
 def test_my_dataset():
     """Test the MyDataset class."""
-    dataset = MyDataset("data/raw")
+    dataset = MyDataset(_PATH_DATA / 'raw_data')
     assert isinstance(dataset, Dataset)
